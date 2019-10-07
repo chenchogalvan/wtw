3b306d7524a60a90ce3579e38db406fe131e9457
Alfredo Galvan <agalvan@clustermx.com>
2019-10-07 01:31:51 -0500

    wtw

---
+++ resources/views/index.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to woof</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+
+    <section class="slider" style="background: url(http://via.placeholder.com/2000x1336)">
+
+    </section>
+
+
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 " style="min-height:75vh;">
+                    <div class="intro-services">
+                        <span class="title-span">Welcome to Woof</span> is a professional and personalized Pet Care
+                        Company licensed, bonded and insured for the safe of your pet, home and property.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section-services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card-img center-card row text-center">
+                        <div class="col-lg-4 mb-20">
+                            <div class="find_out_feature_wrap">
+                                <img src="https://paymentx.io/images/f_ic_01.svg" alt="Set up repeat instructions"
+                                    class="icon-feature">
+                                <h3 class="feature-title">Dog walking</h3>
+                                <p class="feature-desc">Solo and pack walks, 30 minutes, 1 hour</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-20">
+                            <div class="find_out_feature_wrap">
+                                <img src="https://paymentx.io/images/f_ic_01.svg" alt="Set up repeat instructions"
+                                    class="icon-feature">
+                                <h3 class="feature-title">Drop in visits </h3>
+                                <p class="feature-desc">25 minutes visits for cats and small animals</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-20">
+                            <div class="find_out_feature_wrap">
+                                <img src="https://paymentx.io/images/f_ic_01.svg" alt="Set up repeat instructions"
+                                    class="icon-feature">
+                                <h3 class="feature-title">Boarding & Sitting </h3>
+                                <p class="feature-desc">Personalized boarding/sitting in sitters or owner home</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card-img center-card row text-center">
+                        <div class="col-lg-4 mb-20">
+                            <div class="find_out_feature_wrap">
+                                <img src="https://paymentx.io/images/f_ic_01.svg" alt="Set up repeat instructions"
+                                    class="icon-feature">
+                                <h3 class="feature-title">Pet concierge </h3>
+                                <p class="feature-desc">Pet taxi for grooming appointments, vet visits and more</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-20">
+                            <div class="find_out_feature_wrap">
+                                <img src="https://paymentx.io/images/f_ic_01.svg" alt="Set up repeat instructions"
+                                    class="icon-feature">
+                                <h3 class="feature-title">Outdoor excursions</h3>
+                                <p class="feature-desc">Extended group walks, hiking and doggie parks</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-20">
+                            <div class="find_out_feature_wrap">
+                                <img src="https://paymentx.io/images/f_ic_01.svg" alt="Set up repeat instructions"
+                                    class="icon-feature">
+                                <h3 class="feature-title">Doggie Day Care </h3>
+                                <p class="feature-desc">Home-based daycare</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+    <section class="section-about">
+        
+    </section>
+
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
